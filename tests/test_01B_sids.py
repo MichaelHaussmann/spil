6ca5fcc6154d68c11e4f29d3adf1b513f8fcf51f
Michael Haussmann <git@xeo.info>
2021-08-21 08:54:02 +0200

    test enhancements

--- tests/test_01B_sids.py
+++ tests/test_01B_sids.py
@@ -14,10 +14,10 @@ If not, see <https://www.gnu.org/licenses/>.
 """
 import six
 
-from spil import Sid
+from spil import Sid, SpilException
 from tests import test_00_init  # import needed before spil.conf
 
-from spil.util.log import debug, setLevel, INFO, DEBUG, info, ERROR
+from spil.util.log import debug, setLevel, INFO, DEBUG, info, ERROR, error
 
 from spil.conf import sid_templates, path_templates
 from spil.util.log import info
@@ -29,7 +29,7 @@ from pprint import pprint, pformat
 print(test_00_init)
 
 
-def test_sids(sids):
+def test_sids(sids, reraise=True):
 
     info('Testing if example sids match the Sid config')
 
@@ -46,46 +46,60 @@ def test_sids(sids):
         if not s.count('?'):  # Asset works only without URI part
             assert str(sid) == s
         assert sid == eval(repr(sid))
-        assert sid == Sid(sid.get('project') + '?' + sid.get_uri())
 
-        if not sid.type:
-            warn('Sid "{}" not typed, skipping'.format(sid))
-            continue
+        try:
+            assert sid == Sid(sid.get('project') + '?' + sid.get_uri())
 
-        key = sid.keytype
-        parent_key = sid.parent.keytype
+            if not sid.type:
+                warn('Sid "{}" not typed, skipping'.format(sid))
+                continue
 
-        if not sid.type == 'project':
-            if not sid.parent == sid.get_as(parent_key):
-                warn('Sid "{}" parent problem'.format(sid))
+            key = sid.keytype
+            parent_key = sid.parent.keytype
 
-        if not sid.get_last(key):
-            print('Sid "{}" does not return get_last("{}") - probably bad.'.format(sid, key))
+            if not sid.type == 'project':
+                if not sid.parent == sid.get_as(parent_key):
+                    warn('Sid "{}" parent problem'.format(sid))
 
-        path = sid.path
-        if path:
-            assert sid == Sid(path=sid.path)
-            assert sid.path == Sid(path=sid.path).path
-        else:
-            print('Sid "{}" has no path.'.format(sid))
+            if not sid.get_last(key):
+                print('Sid "{}" does not return get_last("{}") - probably bad.'.format(sid, key))
 
-        params = {'parent': sid.parent,
-                  'grand_parent': sid.parent.parent,
-                  'basetype': sid.basetype,
-                  'keytype': sid.keytype,
-                  'path': sid.path,
-                  'exists': sid.exists(),
-                  'is_leaf': sid.is_leaf(),
-                  'len': len(sid),
-                  'get_last': sid.get_last(key),
-                  'type': sid.type,
-                  'full_string': sid.full_string,
-                  'string': sid.string,
-                  'uri': sid.get_uri(),
-                  }
+            path = sid.path
+            if path:
+                assert sid == Sid(path=sid.path)
+                assert sid.path == Sid(path=sid.path).path
+            else:
+                print('Sid "{}" has no path.'.format(sid))
 
-        pprint(params)
-        pprint(sid.data)
+            params = {'parent': sid.parent,
+                      'grand_parent': sid.parent.parent,
+                      'basetype': sid.basetype,
+                      'keytype': sid.keytype,
+                      'path': sid.path,
+                      'exists': sid.exists(),
+                      'is_leaf': sid.is_leaf(),
+                      'len': len(sid),
+                      'get_last': sid.get_last(key),
+                      'get_next': sid.get_next('version'),
+                      'get_new': sid.get_new('version'),
+                      'type': sid.type,
+                      'full_string': sid.full_string,
+                      'string': sid.string,
+                      'uri': sid.get_uri(),
+                      }
+
+            pprint(params)
+            pprint(sid.data)
+
+        except SpilException as e:
+            error('SpilException : {} --> {}'.format(e, s))
+            if reraise:
+                raise e
+
+        except Exception as e:
+            error('Exception : {} --> {}'.format(e, s))
+            if reraise:
+                raise e
 
 
 if __name__ == '__main__':
@@ -113,10 +127,22 @@ if __name__ == '__main__':
 
     failing_uri = ['THING?type=A&cat=PRP', 'FTOT?cat=PRP' ]
 
-    test_sids(working_uri)
+    #test_sids(working_uri)
     # test_sids(failing_uri)
 
     test_sids(sids[:5000])
+
+    new_tasks = ['FTOT/S/SQ0001/SH0020/FX-*', 'FTOT/S/SQ0001/SH0020/*', 'FTOT/S/SQ0001/SH0020/FX', 'FTOT/S/SQ0001/SH0020/FX-MOCCO']
+    # test_sids(new_tasks)
+
+    bad_tasks = ['FTOT/S/SQ0001/SH0020/F', 'FTOT/S/SQ0001/SH0020/FXMOCCO', 'FTOT/S/SQ0001/SH0020/RND-']
+    # test_sids(bad_tasks, reraise=False)
+
+    new_tasks = ['FTOT/A/CHR/MOCCO/CFXin', 'FTOT/A/CHR/MOCCO/CFXsim', 'FTOT/A/CHR/MOCCO/CFXsim-HAT',]
+    test_sids(new_tasks)
+
+    bad_tasks = ['FTOT/S/SQ0001/SH0020/F', 'FTOT/S/SQ0001/SH0020/FXMOCCO', 'FTOT/S/SQ0001/SH0020/RND-']
+    # test_sids(bad_tasks, reraise=False)
 
     print('Done')
 
