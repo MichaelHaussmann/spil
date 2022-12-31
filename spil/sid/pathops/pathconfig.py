d3deaf1364116d3cf812a94eb27dd480f632581b
michael <git@xeo.info>
2022-12-31 19:10:52 +0100

    typing fixes

--- spil/sid/pathops/pathconfig.py
+++ spil/sid/pathops/pathconfig.py
@@ -1,6 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of SPIL, The Simple Pipeline Lib.
+
+(C) copyright 2019-2023 Michael Haussmann, spil@xeo.info
+
+SPIL is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+SPIL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with SPIL.
+If not, see <https://www.gnu.org/licenses/>.
+"""
 from __future__ import annotations
+from typing import Optional
+
 import importlib
 import inspect
+
 from spil import conf
 from spil.util.log import debug
 from spil.conf.util import pattern_replacing
@@ -8,14 +24,14 @@ from spil.util.caching import lru_cache as cache
 
 
 @cache
-def get_path_config(name: str | None = None) -> PathConfig:
+def get_path_config(name: Optional[str] = None) -> PathConfig:
 
     if not name:  # either name, or configured default, or first config_name entry
-        name = conf.default_path_config or list(conf.path_configs.keys())[0]
+        name = conf.default_path_config or list(conf.path_configs.keys())[0]  # type: ignore
 
-    config_module_name = conf.path_configs.get(name)
+    config_module_name = conf.path_configs.get(name)  # type: ignore
 
-    config = PathConfig(name, config_module_name)
+    config = PathConfig(name, config_module_name)  # type: ignore
 
     debug(f'Getting: {config}')
 
@@ -24,13 +40,13 @@ def get_path_config(name: str | None = None) -> PathConfig:
 
 class PathConfig:
 
-    def __init__(self, name: str = 'fs_conf', config_module_name: str = None):
+    def __init__(self, name: Optional[str] = 'fs_conf', config_module_name: Optional[str] = None):
 
         self.name = name
         config_module_name = config_module_name or name
 
         try:
-            self.module = importlib.import_module(config_module_name)
+            self.module = importlib.import_module(config_module_name)  # type: ignore
         except ModuleNotFoundError as e:
             problem = conf.sid_conf_import_error_message.format(module=config_module_name)
             print(problem)
@@ -42,7 +58,7 @@ class PathConfig:
 
             setattr(self, name, value)
 
-        pattern_replacing(self.path_templates, self.key_patterns)
+        pattern_replacing(self.path_templates, self.key_patterns)  # type: ignore
 
     def __str__(self):
         return f"PathConfig: {self.name} / {self.module}"
