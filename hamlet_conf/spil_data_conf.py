39e4de37ffa7a5f0a78fa12e97307f97064c351c
michael <git@xeo.info>
2023-01-04 00:30:09 +0100

    wip

--- hamlet_conf/spil_data_conf.py
+++ hamlet_conf/spil_data_conf.py
@@ -1,44 +1,69 @@
+"""
 
+This is the config file for data access.
+
+It is ingested by spil.conf.data_conf_load, which reads it into spil.conf.
+
+
+*This is work in progress*
+
+The data config specification is subject to change.
+(without affecting spils API).
+
+"""
 # attribute getters
 # cachable_attributes (by getter / by type / with TTL - for example publish file size, date, owner
 
 # sid_cache_path = '/home/mh/PycharmProjects/spil2/hamlet_conf/data/caches'
 # sid_cache_folder = sid_cache_path
 
+# ---------------------------------------------
+# Path config
 path_configs = {'local': 'spil_fs_conf',
                 'server': 'spil_fs_server_conf'
                 }
-
-path_data_suffix = '.data.json'
-
 default_path_config = 'local'
 
-# WriteToPath: create
-# When a Sid that has a suffix (an extension), we want to create a file.
-# If a template exists for the suffix, we copy it.
+# WriteToPaths & GetFromPaths
+path_data_suffix = '.data.json'
+# WriteToPath: create. If a template exists for a suffix (extension), we copy it.
 create_file_using_template = {  # type: ignore
     # 'ma': '.../empty.ma',
     # 'mb': '.../empty.mb'
 }
-# if no template exists, we create an empty file with path.touch(), if create_using_touch is True
+# WriteToPath: create. If no template exists, we create an empty file with path.touch(), if create_using_touch is True
 create_file_using_touch = True
 # If nothing of these is set, we do not create a file.
+# ---------------------------------------------
+# End Path config
 
 
-def get_finder_for(sid, config=None):  # get finder by Sid and optional config
+def get_finder_for(search_sid, config=None):  # get finder by Sid and optional config
     """
-    For a given Sid, looks up the Sid type and the matching data_source, as defined in a dict.
-    Returned value from the config_name is an instance.
+    Configuration used by FindInAll, to define which Finder is used for a given Search Sid.
+
+    For a given Search Sid, and optional config,
+    looks up the Search Sid's type and the matching Finder, as defined in an ad-hoc dict.
+
+    The config allows to create multiple FindInAll instances / behaviours.
+    (similar to the FindInPaths where we Find in different file systems)
+
+    Args:
+        search_sid: sid that is searched by FindInAll
+        config: optional config to be able to define multiple FindInAll instances.
+
+    Returns:
+        A Finder object for this search.
     """
     # type: ignore
-    from spil_sid_conf import projects, asset_types  # , asset_tasks, shot_tasks
+    from spil_sid_conf import projects, asset_types  # type: ignore
     from spil import FindInConstants, FindInPaths
 
     finder_projects = FindInConstants("project", projects)
     finder_types = FindInConstants("type", ["a", "s"], parent_source=finder_projects)
     finder_assettypes = FindInConstants('assettype', asset_types, parent_source=finder_types)
 
-    data_sources = {
+    finders_by_type = {
         'project': finder_projects,
         'asset__type': finder_types,
         'shot__type': finder_types,
@@ -46,37 +71,42 @@ def get_finder_for(sid, config=None):  # get finder by Sid and optional config
         'default': FindInPaths()
     }
 
-    source = data_sources.get(sid.type, {}) or data_sources.get('default', {})
-    #print('Sid {} --> {} --> {}'.format(sid.full_string, sid.type, source))
-    if source:
-        return source
+    finder = finders_by_type.get(search_sid.type, {}) or finders_by_type.get('default', {})
+    if finder:
+        return finder
     else:
-        #print('Data Source not found for Sid "{}" ({})'.format(sid, sid.type))
         return None
 
 
 def get_getter_for(sid, attribute):
     """
-    For a given attribute, looks up the matching attribute_sources, as defined in a dict.
-    Returned value is a function.
+
+    *This is work in progress*
+
+    For a given attribute, looks up the matching attribute_getters, as defined in a dict.
+    Returned value is a getter function.
 
     Currently, the sid argument is not used.
     """
-    # from pipe_action.libs.files import get_comment, get_size, get_time
+    # from spil_action.libs.files import get_comment, get_size, get_time
 
-    attribute_sources = {
+    attribute_getters = {
         #'comment': get_comment,
         #'size': get_size,
         #'time': get_time,
     }
 
-    source = attribute_sources.get(attribute)
-    if source:
-        return source
+    getter = attribute_getters.get(attribute)
+    if getter:
+        return getter
     else:
-        print('Attribute Source not found for Attribute "{}" ({})'.format(attribute, sid))
+        print('No getter found for Attribute "{}" ({})'.format(attribute, sid))
         return None
 
 
-def get_writer_for(sid):  # TODO: implement separate source and destination objects.
-    return conf.get_writer_for(sid)
+def get_writer_for(sid):
+    """
+    *This is work in progress*
+    """
+    raise NotImplemented()
+
