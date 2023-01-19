e0b8714b31efbc719b9ad937dd5059456a199c16
michael <git@xeo.info>
2023-01-19 21:36:34 +0100

    additions to caching and exception for later usage

--- spil/util/caching.py
+++ spil/util/caching.py
@@ -2,7 +2,7 @@
 """
 This file is part of SPIL, The Simple Pipeline Lib.
 
-(C) copyright 2019-2022 Michael Haussmann, spil@xeo.info
+(C) copyright 2019-2023 Michael Haussmann, spil@xeo.info
 
 SPIL is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
@@ -10,23 +10,18 @@ SPIL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY
 
 You should have received a copy of the GNU Lesser General Public License along with SPIL.
 If not, see <https://www.gnu.org/licenses/>.
-
 """
 """
 Thank you Hugh Brown
 https://gist.github.com/hughdbrown
-
 https://gist.github.com/hughdbrown/bf5c63792d5f912a162bf012fb6b4527
-
 Also borrowed pieces from functool.py, Copyright (C) 2006-2013 Python Software Foundation
 
 This is a minimal but fast lru implementation.
 
 TODO: replace for PY3 with the functools.lru_cache implementation.
 Note that the cache size needs to be high enough.
-
 """
-
 from functools import wraps
 
 _max_size = 4096 * 2 * 2  # approx 40 Mo
@@ -92,4 +87,50 @@ def lru_kw_cache(user_function):
     wrapper.cache_clear = cache_clear
     wrapper.cache_info = cache_info
     return wrapper
+
+
+def hit_cache(user_function):
+    """
+    A cache decorator that caches only if the wrapped function returns something.
+    This is useful on data that is often queried but rarely or not at all erased.
+
+    TODO: add TTL mechanism
+
+    Args:
+        user_function:
+
+    Returns:
+
+    """
+    cache = {}
+
+    @wraps(user_function)
+    def wrapper(*args, **kwargs):
+        key = tuple(args) + tuple(kwargs)
+        if key not in cache:
+            # Validate we didn't exceed the max_size:
+            if len(cache) >= _max_size:
+                # Delete an item in the dict:
+                # print('lru_cache over size')
+                cache.popitem()  # does not make sens in PY2
+                # cache.clear()
+            returned = user_function(*args, **kwargs)
+            if returned:
+                cache[key] = returned
+            else:
+                return returned
+        return cache[key]
+
+    def cache_clear():
+        cache.clear()
+
+    def cache_info():
+        from pprint import pformat
+        return pformat(cache)
+
+    wrapper.cache_clear = cache_clear
+    wrapper.cache_info = cache_info
+    return wrapper
+
+
 
