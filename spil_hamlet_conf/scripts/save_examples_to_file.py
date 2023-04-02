57165219f777a5c09862fb8b2aa6433c3fccac39
michael <git@xeo.info>
2023-04-02 09:19:42 +0200

    use default_sid_conf_data_path

--- spil_hamlet_conf/scripts/save_examples_to_file.py
+++ spil_hamlet_conf/scripts/save_examples_to_file.py
@@ -6,11 +6,11 @@ This test file will be read in test scripts.
 
 """
 from pathlib import Path
-
+from spil import conf
 from scripts.generate_example_sids import sids  # type: ignore
 from spil.tests.prep.save_sid_list_to_file import write_sids_to_file
 
-sid_file = Path(__file__).parent.parent / "data" / "testing" / "hamlet.sids.txt"
+sid_file = Path(conf.default_sid_conf_data_path) / "testing" / "hamlet.sids.txt"
 
 if not sid_file.exists():
     print(f"Creating Sid test file: {sid_file}")
