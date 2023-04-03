bee4ed8a73db33e88feddd12a8934691aaf041f0
michael <git@xeo.info>
2023-04-03 23:35:45 +0200

    fixed documentation and bootstrap with demo config

--- spil_hamlet_conf/hamlet_scripts/save_examples_to_mock_fs.py
+++ spil_hamlet_conf/hamlet_scripts/save_examples_to_mock_fs.py
@@ -1,13 +1,28 @@
 # type: ignore
+# noqa
 """
 This script uses the test sids,
 and generates empty but correctly named files,
 according to the path configs.
 
 """
+from contextlib import suppress
+
 import spil_data_conf
+from spil import SpilException
 from hamlet_scripts.example_sids import sids
 from spil.tests.prep.make_mock_fs import generate_mock_fs
+
+def run():
+
+    print("Starting generate_mock_fs.run()")
+
+    for config in list(spil_data_conf.path_configs):
+        with suppress(SpilException):  # ignore exception if already exists
+            generate_mock_fs(sids, config=config)
+
+    print("Done generate_mock_fs.run()")
+
 
 if __name__ == "__main__":
 
@@ -21,5 +36,4 @@ if __name__ == "__main__":
     setLevel(INFO)
     # setLevel(DEBUG)  # In case of problems, use DEBUG mode
 
-    for config in list(spil_data_conf.path_configs):
-        generate_mock_fs(sids, config=config)
+    run()
