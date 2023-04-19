f0c2ccc5e8d44fa82f03c37fee4b0708b5817ae3
Michael Haussmann <git@xeo.info>
2023-04-19 17:48:31 +0200

    basic rez package

---
+++ package.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+name = 'spil'
+
+version = '0.1.1'
+
+requires = [
+    "Fileseq",
+    "future",
+    "logzero",
+    "typing_extensions"
+]
+
+description = "https://github.com/MichaelHaussmann/spil"
+authors = ['Michael Haussmann']
+
+
+def commands():
+    env.PYTHONPATH.append('{root}')
+
+
+is_pure_python = True
