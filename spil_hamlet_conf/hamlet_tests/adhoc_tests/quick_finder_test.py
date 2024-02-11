8fbbd65973c7312d028b81d7918ff4871945dd2b
“Michael <git@xeo.info>
2024-02-11 23:55:30 +0100

    Bugfix FindInPaths was missing results with same pattern for different types

--- spil_hamlet_conf/hamlet_tests/adhoc_tests/quick_finder_test.py
+++ spil_hamlet_conf/hamlet_tests/adhoc_tests/quick_finder_test.py
@@ -1,12 +1,27 @@
-from spil import FindInPaths, Sid
+"""
+Debug for specific search results.
 
+hamlet/a/*/claudius/rig/v001/*/*
+hamlet/a/*/claudius/rig/*/w/*
+hamlet/*/char/claudius/*/v001/w/*
+
+"""
+import spil
+from hamlet_scripts.example_sids import sids  # type: ignore
+from spil import FindInList, FindInPaths
+
+# finder = FindInList(sids)
 finder = FindInPaths("server")
 
 search = 'hamlet/a/*/claudius/rig/*/w/*'
-print('*'*50)
-for f in finder.find(search, as_sid=False):
-    print(f)
-print('*'*50)
+# search = 'hamlet/*/char/claudius/*/v001/w/*'
+# search = 'hamlet/a/*/claudius/rig/*/w/**'
+
+for i in range(1):
+    print('*'*50)
+    for f in finder.find(search):
+        print(f)
+    print('*'*50)
 #
 # for f in finder.find(search):
 #     print(f)
